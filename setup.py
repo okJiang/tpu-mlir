f10042394be31e92c4dbeee1858b3c12a8aeadc6
okJiang <819421878@qq.com>
2023-09-28 23:57:38 +0800

    update

--- setup.py
+++ setup.py
@@ -10,54 +10,53 @@ import subprocess
 def collect_caffe_dependence():
 
 
-    os.system("cp /usr/lib/x86_64-linux-gnu/libpython3.10.so.1.0 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_thread.so.1.74.0 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.74.0 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_regex.so.1.74.0 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libglog.so.0 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libgflags.so.2.2 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libprotobuf.so.23 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libm.so.6 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_python310.so.1.74.0 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libgomp.so.1 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libz.so.1 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libicui18n.so.70 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libicuuc.so.70 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libunwind.so.8 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libpthread.so.0  /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libgfortran.so.5  /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/liblzma.so.5 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libquadmath.so.0 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libopenblas.so.0  /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libgcc_s.so.1  /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libicudata.so.70  /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/lib/x86_64-linux-gnu/libc.so.6 /workspace/tpu-mlir/install/lib")
-    os.system("cp -r /usr/local/python_packages/caffe /workspace/tpu-mlir/install")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libpython3.10.so.1.0 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_thread.so.1.74.0 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.74.0 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_regex.so.1.74.0 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libglog.so.0 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libgflags.so.2.2 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libprotobuf.so.23 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libm.so.6 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libboost_python310.so.1.74.0 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libgomp.so.1 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libz.so.1 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libicui18n.so.70 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libicuuc.so.70 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libunwind.so.8 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libpthread.so.0  /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libgfortran.so.5  /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/liblzma.so.5 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libquadmath.so.0 /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libopenblas.so.0  /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libgcc_s.so.1  /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libicudata.so.70  /workspace/install/lib")
+    os.system("cp /usr/lib/x86_64-linux-gnu/libc.so.6 /workspace/install/lib")
+    os.system("cp -r /usr/local/python_packages/caffe /workspace/install")
     print("caffe dependence cllect Done")
 
 def collect_oneDNN_dependence():
-    os.system("cp /usr/local/lib/libdnnl.so /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/local/lib/libdnnl.so.3 /workspace/tpu-mlir/install/lib")
-    os.system("cp /usr/local/lib/libdnnl.so.3.1 /workspace/tpu-mlir/install/lib")
+    os.system("cp /usr/local/lib/libdnnl.so /workspace/install/lib")
+    os.system("cp /usr/local/lib/libdnnl.so.3 /workspace/install/lib")
+    os.system("cp /usr/local/lib/libdnnl.so.3.1 /workspace/install/lib")
     print("oneDNN dependence cllect Done")
 
 
 def release_packages_merge():
     os.system("mkdir tpu_mlir_release")
-    #os.system("cp -r /workspace/tpu-mlir/install/lib  /workspace/tpu-mlir/tpu_mlir_release")
-    os.system("find /workspace/tpu-mlir/install/lib/ -type f -name '*.so*' -exec cp {}  /workspace/tpu-mlir/tpu_mlir_release \;")
-    os.system("find /workspace/tpu-mlir/install/lib/ -type f -name '*.so.*' -exec cp {}  /workspace/tpu-mlir/tpu_mlir_release \;")
-    os.system("cp -r /workspace/tpu-mlir/install/python  /workspace/tpu-mlir/tpu_mlir_release")
-   # os.system("cp -r /workspace/tpu-mlir/install/docs  /workspace/tpu-mlir/tpu_mlir_release")
-    os.system("cp -r /workspace/tpu-mlir/install/src   /workspace/tpu-mlir/tpu_mlir_release")
-    os.system("cp -r /workspace/tpu-mlir/install/bin  /workspace/tpu-mlir/tpu_mlir_release")
-    os.system("cp -r /workspace/tpu-mlir/regression  /workspace/tpu-mlir/tpu_mlir_release")
-    os.system("cp -r /workspace/tpu-mlir/third_party/customlayer  /workspace/tpu-mlir/tpu_mlir_release")
-    os.system("cp -r /workspace/tpu-mlir/install/caffe   /workspace/tpu-mlir/tpu_mlir_release")
-    os.system("mkdir /workspace/tpu-mlir/tpu_mlir_release/lib")
-    os.system("find /workspace/tpu-mlir/tpu_mlir_release/ -type f -name '*.so.*' -exec mv {}  /workspace/tpu-mlir/tpu_mlir_release/lib \;")
-    os.system("find /workspace/tpu-mlir/tpu_mlir_release/ -type f -name '*.so*' -exec mv {}  /workspace/tpu-mlir/tpu_mlir_release/lib \;")
+    os.system("mkdir /workspace/tpu_mlir_release/lib")
+
+    #os.system("cp -r /workspace/install/lib  /workspace/tpu_mlir_release")
+    os.system("find /workspace/ -type f -name '*.so*' -exec cp {}  /workspace/tpu_mlir_release/lib \;")
+    os.system("find /workspace/ -type f -name '*.so.*' -exec cp {}  /workspace/tpu_mlir_release/lib \;")
+    os.system("cp -r /workspace/install/python  /workspace/tpu_mlir_release")
+   # os.system("cp -r /workspace/install/docs  /workspace/tpu_mlir_release")
+    os.system("cp -r /workspace/install/src   /workspace/tpu_mlir_release")
+    os.system("cp -r /workspace/install/bin  /workspace/tpu_mlir_release")
+    os.system("cp -r /workspace/regression  /workspace/tpu_mlir_release")
+    os.system("cp -r /workspace/third_party/customlayer  /workspace/tpu_mlir_release")
+    os.system("cp -r /workspace/install/caffe   /workspace/tpu_mlir_release")
     print("release packages merge Done")
 
 
@@ -73,7 +72,7 @@ def release_packages_merge():
 
 def show_readmefile():
 
-    here = os.path.abspath(os.path.dirname("/workspace/tpu-mlir/"))
+    here = os.path.abspath(os.path.dirname("/workspace/"))
     with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
         long_description = "\n" + fh.read()
     print("show README file Done")
@@ -99,14 +98,14 @@ def search_files(dir_path):
 
 def tpu_mlir_setup():
 
-    list1 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/"))
-    # list2 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/regression"))
-    # list3 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/lib"))
-    # list4 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/docs"))
-    # list5 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/src"))
-    # list6 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/bin"))
-    # list7 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/customlayer"))
-    # list8 = list(search_files("/workspace/tpu-mlir/tpu_mlir_release/caffe"))
+    list1 = list(search_files("/workspace/tpu_mlir_release/"))
+    # list2 = list(search_files("/workspace/tpu_mlir_release/regression"))
+    # list3 = list(search_files("/workspace/tpu_mlir_release/lib"))
+    # list4 = list(search_files("/workspace/tpu_mlir_release/docs"))
+    # list5 = list(search_files("/workspace/tpu_mlir_release/src"))
+    # list6 = list(search_files("/workspace/tpu_mlir_release/bin"))
+    # list7 = list(search_files("/workspace/tpu_mlir_release/customlayer"))
+    # list8 = list(search_files("/workspace/tpu_mlir_release/caffe"))
 
 
     setup(
@@ -139,12 +138,12 @@ def tpu_mlir_setup():
 
 
 def remove_tpu_mlir_release():
-    os.system("rm -r /workspace/tpu-mlir/tpu_mlir_release")
+    os.system("rm -r /workspace/tpu_mlir_release")
 
 def workflow():
   #  show_readmefile()
-    collect_caffe_dependence()
-    collect_oneDNN_dependence()
+    # collect_caffe_dependence()
+    # collect_oneDNN_dependence()
     release_packages_merge()
     tpu_mlir_setup()
    # remove_tpu_mlir_release()
